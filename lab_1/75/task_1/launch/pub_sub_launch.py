b1b1138c6d99da7a849f057ecbee4d406b624b33
Alexa Barron <barron49@purdue.edu>
2026-09-02 16:20:30 -0400

    Launch file added

---
+++ lab_1/75/task_1/launch/pub_sub_launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(package='task_1',
+            executable='talker',
+            name='talker',
+        ),
+        Node(package='task_1',
+            executable='listener',
+            name='listener',
+        ),
+    ])
